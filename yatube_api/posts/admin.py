527f0af7f8503d3a11939c6de9811d7d097f56a1
Timur Gain <timur.gain@gmail.com>
2022-02-01 18:29:41 +0300

    models, migrations, admin

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Group, Comment, Follow
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-'
+    list_editable = ('group',)
+
+
+admin.site.register(Group)
+admin.site.register(Comment)
+admin.site.register(Follow)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,22 +5,114 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    """Table with posts."""
+    text = models.TextField(
+        help_text='Начните здесь статью',
+        verbose_name='Статья',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации',
+    )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор статьи',
+    )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        upload_to='posts/', null=True, blank=True,
+        help_text='Загрузите картинку',
+        verbose_name='Картинка для статьи',
+    )
+    group = models.ForeignKey(
+        to='Group',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        help_text='Выбери группу/сообщество',
+        verbose_name='Сообщество',
+    )
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
 
     def __str__(self):
-        return self.text
+        return f"{self.id}: {self.text[0:30]}"
+
+
+class Group(models.Model):
+    """Table with groups."""
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Сообщество',
+    )
+    slug = models.SlugField(
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+
+    class Meta:
+        verbose_name = 'Сообщество'
+        verbose_name_plural = 'Сообщества'
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
+    """Table with comments."""
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария',
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        to=Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Комментарий',
+    )
+    text = models.TextField(
+        verbose_name='Комментарий',
+    )
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self) -> str:
+        return f"{self.text[0:30]}..., {self.author}"
+
+
+class Follow(models.Model):
+    """Table with subscriptions."""
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='folloing',
+        verbose_name='Автор',
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self) -> str:
+        return f'{self.user} подписан на автора {self.author}'
